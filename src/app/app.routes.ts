44d4baa9427c7361e22071a38a541b937a26e72b
sslcristian <catencia1@udi.edu.co>
2025-09-14 11:50:57 -0500

    create component "inicio"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './inicio/inicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' }, // redirección al inicio
+  { path: 'inicio', component: Inicio }
+];
