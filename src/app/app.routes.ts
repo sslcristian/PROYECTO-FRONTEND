6e876bfee05c6918863f0d637227e56e7fc67bb6
sslcristian <catencia1@udi.edu.co>
2025-10-03 10:02:41 -0500

    ATENCIA
    NUEVAS CARPETAS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Inicio } from './inicio/inicio';
+import { Inicio } from './paginas/inicio/inicio';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' }, // redirección al inicio
